c2db177e5218d5986204444663f5849dc52bd301
jstaffo3 <jstaffo3@lion.lmu.edu>
2018-12-03 12:04:13 +0100

    Added gameInitialize.js

--- playerMovement.js
+++ playerMovement.js
@@ -1,15 +1,18 @@
 let keys = game.input.keyboard();
-let speed = playerSpeedBase * playerSpeedModifier;
 
-if (keys.isDown(87)) {
-    player.y -= speed;
-}
-if (keys.isDown(83)) {
-    player.y += speed;
-}
-if (keys.isDown(65)) {
-    player.x -= speed;
-}
-if (keys.isDown(68)) {
-    player.x += speed;
+function playerMovement() {
+    let speed = playerSpeedBase * playerSpeedModifier;
+
+    if (keys.isDown(87)) {
+        player.y -= speed;
+    }
+    if (keys.isDown(83)) {
+        player.y += speed;
+    }
+    if (keys.isDown(65)) {
+        player.x -= speed;
+    }
+    if (keys.isDown(68)) {
+        player.x += speed;
+    }
 }
