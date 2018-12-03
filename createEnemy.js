33c853ff6766ec4ad4c46ab8a7f88aa6d5f4a629
jstaffo3 <jstaffo3@lion.lmu.edu>
2018-12-03 18:29:38 +0100

    Added John sprite

---
+++ createEnemy.js
@@ -0,0 +1,11 @@
+class Enemy {
+    constructor(sprite) {
+        this.sprite = sprite;
+        game.add.sprite(game.world.randomX, game.world.randomY, this.sprite);
+        this.speed = Math.random() * 150 + 20;
+    }
+    move() {
+        this.position.x += (charKai.position.x - this.position.x) / this.speed;
+        this.position.y += (charKai.position.y - this.position.y) / this.speed;
+    }
+}
