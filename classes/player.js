9d463acd2b4fc3984a6050e7be3fe3c8597aa61c
Matthew <mlow2@lion.lmu.edu>
2018-12-06 09:51:29 +0100

    Make player move at same rate diagonally. Revisit later to improve

--- classes/player.js
+++ classes/player.js
@@ -6,17 +6,24 @@ class Player {
         this.health = 100;
     }
     move() {
+		//let speed = Math.hypot(this.sprite.body.velocity.x,this.sprite.body.velocity.y);
         this.sprite.body.setZeroVelocity();
-        let speed = playerSpeedBase * playerSpeedModifier;
+        playerSpeedModifier = 1;
+        
+        if (((cursors.up.isDown || wasd.up.isDown)||(cursors.down.isDown || wasd.down.isDown))&&((cursors.left.isDown || wasd.left.isDown)||(cursors.right.isDown || wasd.right.isDown))){
+			playerSpeedModifier = Math.cos(Math.PI/4);
+		}
+		let speed = playerSpeedBase * playerSpeedModifier;
+        
         if (cursors.up.isDown || wasd.up.isDown) {
-            this.sprite.body.moveUp(speed);
+			this.sprite.body.velocity.y = -speed;
         } else if (cursors.down.isDown || wasd.down.isDown) {
-            this.sprite.body.moveDown(speed);
+			this.sprite.body.velocity.y = speed;
         }
         if (cursors.left.isDown || wasd.left.isDown) {
-            this.sprite.body.moveLeft(speed);
+			this.sprite.body.velocity.x = -speed;
         } else if (cursors.right.isDown || wasd.right.isDown) {
-            this.sprite.body.moveRight(speed);
+			this.sprite.body.velocity.x = speed;
         }
     }
 }
